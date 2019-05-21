16ac44dd4287819b2a8eba70678b408b5b4ee9e7
Aaron Turner <aaron@aaronthedev.com>
2019-05-21 10:09:55 -0700

    Added CSS Minification, and linting

--- build-system/build.js
+++ build-system/build.js
@@ -1,40 +1,43 @@
-const fs = require('fs');
-const mkdirp = require('mkdirp');
-const recursive = require('recursive-readdir');
-const highlightJs = require('highlight.js');
-const Mustache = require('mustache');
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const recursive = require("recursive-readdir");
+const highlightJs = require("highlight.js");
+const Mustache = require("mustache");
+const CleanCSS = require("clean-css");
 
-console.log('Building...');
+console.log("Building...");
+
+const cssMinifier = new CleanCSS();
+const minifyCss = filePath => {
+  return cssMinifier.minify(fs.readFileSync(filePath, "utf8")).styles;
+};
 
 const mustacheData = {
-  normalizeCss: fs.readFileSync('node_modules/normalize.css/normalize.css', 'utf8'),
-  sakuraCss: fs.readFileSync('node_modules/sakura.css/css/sakura-dark.css', 'utf8'),
-  indexCss: fs.readFileSync('shell/index.css', 'utf8')
-}
+  normalizeCss: minifyCss("node_modules/normalize.css/normalize.css"),
+  sakuraCss: minifyCss("node_modules/sakura.css/css/sakura-dark.css"),
+  indexCss: minifyCss("shell/index.css")
+};
 
 const readExamplesPromise = new Promise(resolve => {
   // Find all HTML Files within the demo directory, that are not specified
-  recursive('./examples', ['**/demo/*']).then((files) => {
+  recursive("./examples", ["**/demo/*"]).then(files => {
     resolve(files);
   });
 });
 
 const buildTask = async () => {
-
   // Create our build output folder
-  mkdirp.sync('./dist');
+  mkdirp.sync("./dist");
 
   // Create our landing page
-  const index = fs.readFileSync('shell/index.html', 'utf8').toString();
+  const index = fs.readFileSync("shell/index.html", "utf8").toString();
   const renderedIndex = Mustache.render(index, mustacheData);
-  fs.writeFileSync('./dist/index.html', renderedIndex);
+  fs.writeFileSync("./dist/index.html", renderedIndex);
 
   // Create an object for each file that we found, and assign a filepath and name
   const exampleFiles = await readExamplesPromise;
-  exampleFiles.forEach(filePath => {
-  });  
+  exampleFiles.forEach(filePath => {});
 
-  console.log('Done!');
+  console.log("Done!");
 };
 buildTask();
-
