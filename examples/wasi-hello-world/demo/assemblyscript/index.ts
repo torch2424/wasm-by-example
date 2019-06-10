82eda8167aece58bcd15daa95641129acfd4d7c4
Aaron Turner <aaron@aaronthedev.com>
2019-06-09 17:07:10 -0700

    Started the wasi example

--- examples/wasi-hello-world/demo/assemblyscript/index.ts
+++ examples/wasi-hello-world/demo/assemblyscript/index.ts
@@ -1,16 +1,27 @@
-// Set up our memory
-// By growing our Wasm Memory by 1 page (64KB)
-memory.grow(1);
+// This example depends on the AssemblyScript Runtime:
+// https://docs.assemblyscript.org/details/runtime
 
-// Store the value 24 at index 0
-const index = 0;
-const value = 24;
-store<u8>(index, value);
+// Import from the AssemblyScript Wasi bindings
+import { fd_write } from "bindings/wasi";
 
-// Export a function that will read wasm memory
-// and return the value at index 1
-export function readWasmMemoryAndReturnIndexOne(): i32 {
-  // Read the value at indexOne
-  let valueAtIndexOne = load<u8>(1);
-  return valueAtIndexOne;
+function writeToStdOut(fileDescriptor: fd, value: string): void {
+  let s_utf8_len: usize = value.lengthUTF8 - 1;
+  let s_utf8 = value.toUTF8();
+  let iov = memory.allocate(4 * sizeof<usize>());
+  store<u32>(iov, s_utf8);
+  store<u32>(iov + sizeof<usize>(), s_utf8_len);
+  let lf = memory.allocate(1);
+  store<u8>(lf, 10);
+  store<u32>(iov + sizeof<usize>() * 2, lf);
+  store<u32>(iov + sizeof<usize>() * 3, 1);
+  let written_ptr = memory.allocate(sizeof<usize>());
+  fd_write(fileDescriptor, iov, 2, written_ptr);
+  memory.free(written_ptr);
+  memory.free(s_utf8);
 }
+
+function consoleLog(value: string): void {
+  writeToStdOut(value);
+}
+
+consoleLog("Hello Wasi!");
