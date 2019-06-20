6dae53e80cbc2e2b8a2c4395cd0b4132a218b14e
Aaron Turner <aaron@aaronthedev.com>
2019-06-20 01:04:32 -0700

    Started rust audio, but falling asleep

--- examples/audio/demo/rust/index.js
+++ examples/audio/demo/rust/index.js
@@ -1,74 +1,10 @@
-import wasmInit from "./pkg/graphics.js";
+import wasmInit from "./pkg/audio.js";
 
 const runWasm = async () => {
   // Instantiate our wasm module
-  const rustWasm = await wasmInit("./pkg/graphics_bg.wasm");
+  const rustWasm = await wasmInit("./pkg/audio_bg.wasm");
 
   // Create a Uint8Array to give us access to Wasm Memory
   const wasmByteMemoryArray = new Uint8Array(rustWasm.memory.buffer);
-
-  // Get our canvas element from our index.html
-  const canvasElement = document.querySelector("canvas");
-
-  // Set up Context and ImageData on the canvas
-  const canvasContext = canvasElement.getContext("2d");
-  const canvasImageData = canvasContext.createImageData(
-    canvasElement.width,
-    canvasElement.height
-  );
-
-  // Clear the canvas
-  canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-  // Write some functions to get a color value
-  // for either the darker squares and lighter squares
-  const getDarkValue = () => {
-    return Math.floor(Math.random() * 100);
-  };
-  const getLightValue = () => {
-    return Math.floor(Math.random() * 127) + 127;
-  };
-
-  const drawCheckerBoard = () => {
-    const checkerBoardSize = 20;
-
-    // Generate a new checkboard in wasm
-    rustWasm.generate_checker_board(
-      getDarkValue(),
-      getDarkValue(),
-      getDarkValue(),
-      getLightValue(),
-      getLightValue(),
-      getLightValue()
-    );
-
-    // Create a Uint8Array to give us access to Wasm Memory
-    const wasmByteMemoryArray = new Uint8Array(rustWasm.memory.buffer);
-
-    // Pull out the RGBA values from Wasm memory
-    // Starting at the memory index of out output buffer (given by our pointer)
-    // 20 * 20 * 4 = checkboard max X * checkerboard max Y * number of pixel properties (R,G.B,A)
-    const outputPointer = rustWasm.get_output_buffer_pointer();
-    const imageDataArray = wasmByteMemoryArray.slice(
-      outputPointer,
-      outputPointer + checkerBoardSize * checkerBoardSize * 4
-    );
-
-    // Set the values to the canvas image data
-    canvasImageData.data.set(imageDataArray);
-
-    // Clear the canvas
-    canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-    // Place the new generated checkerboard onto the canvas
-    canvasContext.putImageData(canvasImageData, 0, 0);
-  };
-
-  // Lastly, call our function to draw a checkerboard
-  // And run this once every second
-  drawCheckerBoard();
-  setInterval(() => {
-    drawCheckerBoard();
-  }, 1000);
 };
 runWasm();
--- examples/audio/demo/rust/src/lib.rs
+++ examples/audio/demo/rust/src/lib.rs
@@ -1,26 +1,31 @@
 // Add the wasm-pack crate
 use wasm_bindgen::prelude::*;
 
-// Define the size of our "checkerboard"
-const CHECKERBOARD_SIZE: usize = 20;
+// Define our number of samples we handle at once
+const NUMBER_OF_SAMPLES: usize = 1024;
 
-/*
- * 1. What is going on here?
- * Create a static mutable byte buffer. 
- * We will use for putting the output of our graphics,
- * to pass the output to js.
- * NOTE: global `static mut` means we will have "unsafe" code
- * but for passing memory between js and wasm should be fine.
- *
- * 2. Why is the size CHECKERBOARD_SIZE * CHECKERBOARD_SIZE * 4?
- * We want to have 20 pixels by 20 pixels. And 4 colors per pixel (r,g,b,a)
- * Which, the Canvas API Supports.
- */
-const OUTPUT_BUFFER_SIZE: usize = CHECKERBOARD_SIZE * CHECKERBOARD_SIZE * 4;
-static mut OUTPUT_BUFFER: [u8; OUTPUT_BUFFER_SIZE] = [0; OUTPUT_BUFFER_SIZE];
+// Create a static mutable byte buffers. 
+// We will use these for passing audio samples from 
+// javascript to wasm, and from wasm to javascript
+// NOTE: global `static mut` means we will have "unsafe" code
+// but for passing memory between js and wasm should be fine.
+static mut INPUT_BUFFER: [u8; NUMBER_OF_SAMPLES] = [0; NUMBER_OF_SAMPLES];
+static mut OUTPUT_BUFFER: [u8; NUMBER_OF_SAMPLES] = [0; NUMBER_OF_SAMPLES];
 
-// Function to return a pointer to our buffer
-// in wasm memory
+// Function to return a pointer to our 
+// output buffer in wasm memory
+#[wasm_bindgen]
+pub fn get_input_buffer_pointer() -> *const u8 {
+    let pointer: *const u8;
+    unsafe {
+        pointer = INPUT_BUFFER.as_ptr();
+    }
+
+    return pointer;
+}
+
+// Function to return a pointer to our 
+// output buffer in wasm memory
 #[wasm_bindgen]
 pub fn get_output_buffer_pointer() -> *const u8 {
     let pointer: *const u8;
@@ -31,59 +36,35 @@ pub fn get_output_buffer_pointer() -> *const u8 {
     return pointer;
 }
 
-// Function to generate our checkerboard, pixel by pixel
+// Function to do the amplification.
+// By taking the samples currently in the input buffer
+// amplifying them, and placing the result in the output buffer
 #[wasm_bindgen]
-pub fn generate_checker_board(
-    dark_value_red: u8,
-    dark_value_green: u8,
-    dark_value_blue: u8,
-    light_value_red: u8,
-    light_value_green: u8,
-    light_value_blue: u8
-    ) {
+pub fn amplify_audio() {
 
-    // Since Linear memory is a 1 dimensional array, but we want a grid
-    // we will be doing 2d to 1d mapping
-    // https://softwareengineering.stackexchange.com/questions/212808/treating-a-1d-data-structure-as-2d-grid
-    for y in 0..CHECKERBOARD_SIZE {
-        for x in 0..CHECKERBOARD_SIZE {
-            // Set our default case to be dark squares
-            let mut is_dark_square: bool = true;
+    // Loop over the samples
+    for i in 0..NUMBER_OF_SAMPLES {
+        // Load the sample at the index
+        let mut audio_sample: u8;
+        unsafe {
+            audio_sample = INPUT_BUFFER[i];
+        }
 
-            // We should change our default case if
-            // We are on an odd y
-            if y % 2 == 0 {
-                is_dark_square = false;
-            }
+        // Amplify the sample. All samples
+        // Should be implemented as bytes.
+        // Byte samples are represented as follows:
+        // 127 is silence, 0 is negative max, 256 is positive max
+        if audio_sample > 127 {
+            let audio_sample_diff = audio_sample - 127;
+            audio_sample = audio_sample + audio_sample_diff;
+        } else if audio_sample < 127 {
+            audio_sample = audio_sample / 2;
+        }
 
-            // Lastly, alternate on our x value
-            if x % 2 == 0 {
-                is_dark_square = !is_dark_square;
-            }
-
-            // Now that we determined if we are dark or light,
-            // Let's set our square value
-            let mut square_value_red: u8 = dark_value_red;
-            let mut square_value_green: u8 = dark_value_green;
-            let mut square_value_blue: u8 = dark_value_blue;
-            if !is_dark_square {
-                square_value_red = light_value_red;
-                square_value_green = light_value_green;
-                square_value_blue = light_value_blue;
-            }
-
-            // Let's calculate our index, using our 2d -> 1d mapping.
-            // And then multiple by 4, for each pixel property (r,g,b,a).
-            let square_number: usize = y * CHECKERBOARD_SIZE + x;
-            let square_rgba_index: usize = square_number * 4;
-
-            // Finally store the values.
-            unsafe {
-                OUTPUT_BUFFER[square_rgba_index + 0] = square_value_red; // Red
-                OUTPUT_BUFFER[square_rgba_index + 1] = square_value_green; // Green
-                OUTPUT_BUFFER[square_rgba_index + 2] = square_value_blue; // Blue
-                OUTPUT_BUFFER[square_rgba_index + 3] = 255; // Alpha (Always Opaque)
-            }
+        // Store the audio sample into our output buffer
+        unsafe {
+            OUTPUT_BUFFER[i] = audio_sample;
         }
     }
 }
+
