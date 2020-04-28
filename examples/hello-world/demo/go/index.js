5d7eb77dfc94cc34ea36aa4270796529459a194c
Aaron Turner <aaron@aaronthedev.com>
2020-04-28 14:25:46 -0700

    Got a buffer example working

--- examples/hello-world/demo/go/index.js
+++ examples/hello-world/demo/go/index.js
@@ -10,15 +10,29 @@ const runWasmAdd = async () => {
   const importObject = go.importObject;
   importObject.env["./main.go.jsadd"] = jsAdd;
 
-  console.log(importObject);
-
   // Instantiate our wasm module
   const wasmModule = await wasmBrowserInstantiate("./main.wasm", importObject);
 
-  console.log("yooo");
+  console.log("exports", wasmModule.instance.exports);
+
+  // You have to run the go instance before doing anything else, or else println and things won't work
+  go.run(wasmModule.instance);
 
   // Call the Add function export from wasm, save the result
   const addResult = wasmModule.instance.exports.add(24, 24);
+
+  const bufferPointer = wasmModule.instance.exports.getWasmMemoryBufferPointer();
+
+  console.log(bufferPointer);
+
+  const wasmMemory = new Uint8Array(wasmModule.instance.exports.memory.buffer);
+  wasmMemory[bufferPointer] = 24;
+
+  // console.log(wasmMemory);
+
+  // console.log(wasmMemory[bufferPointer]);
+
+  wasmModule.instance.exports.logBuffer();
 
   // Set the result onto the body
   document.body.textContent = `Hello World! addResult: ${addResult}`;
--- examples/hello-world/demo/go/main.go
+++ examples/hello-world/demo/go/main.go
@@ -1,5 +1,7 @@
 package main
 
+var buffer [10]uint8;
+
 func jsadd(x, y int) int
 
 func main() {
@@ -9,4 +11,25 @@ func main() {
 //go:export add
 func add(x int, y int) int {
     return x + y;
+}
+
+//go:export getWasmMemoryBufferPointer
+func getWasmMemoryBufferPointer() *[10]uint8 {
+  bufferPointer := &buffer;
+  (*bufferPointer)[0] = 12;
+  (*bufferPointer)[1] = 12;
+  println("buffer 0:", buffer[0]);
+  println("buffer 1:", buffer[1]);
+  return bufferPointer;
+}
+
+//go:export logBuffer
+func logBuffer() {
+  println("buffer pointer:", &buffer);
+
+  bufferPointer := &buffer;
+  println("buffer 0:", (*bufferPointer)[0]);
+  println("buffer 1:", buffer[1]);
+  println("buffer 2:", buffer[2]);
+  println("buffer 3:", buffer[3]);
 }
