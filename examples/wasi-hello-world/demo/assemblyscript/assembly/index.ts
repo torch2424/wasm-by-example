d2fb0197f277d059634ead998eb35960922c6980
iulian-birlica <iulian.birlica@pentest-tools.com>
2023-02-07 22:00:30 +0200

    Commented wasi import, because AssemblyScript no longer requires it

--- examples/wasi-hello-world/demo/assemblyscript/assembly/index.ts
+++ examples/wasi-hello-world/demo/assemblyscript/assembly/index.ts
@@ -1,6 +1,8 @@
 // As of AssemblyScript 0.10.0, adding `import "wasi"`, will automatically
 // import WASI bindings, and add some nice defaults for compiling to WASI.
-import "wasi";
+// As of AssemblyScript 0.20.0, `import "wasi"` is no longer necessary. Please check
+// you version and either decomment the following line or leave it commented.
+//import "wasi";
 
 // Import Console (for writing to stdout), and FileSystem (for reading/writing files)
 // from "as-wasi". An API for working with WASI in AssemblyScript much easier.
